698aa9a8e9131c1555943b40fb82e8b6ec626a3e
Murilo Prianti <murilo.silva@sou.inteli.edu.br>
2024-10-10 00:03:09 -0300

    começamos com um forms

--- src/Main.py
+++ src/Main.py
@@ -6,7 +6,14 @@ st.set_page_config(
     
     layout='wide')
 
-
+with st.form(key="Clientes", clear_on_submit=True):
+    grupo = st.text_input("Grupo: ", placeholder="Digite o nome do grupo")
+    nome = st.text_input("Nome: ", placeholder="Digite seu nome aqui")
+    idade = st.number_input("Idade:", format="%d", step=1, min_value=0)
+    st.write("Coloque conforme a sua cahve PIX mesmo, que seja CPF, CNPJ, E-mail ou Telefone")
+    pix = st.text_input("Pix:", placeholder="Digite seu pix")
+    # pix = st.selectbox("Pix:", ("CPF", "Telefone", "E-mail", "CNPJ"))
+    button = st.form_submit_button("Cadastrar")
 
 
 
